097663270d2b74dbcb526e5b10076705e940c0ec
Akash Tyagi <akashdktyagi@gmail.com>
2024-05-01 20:50:09 +0800

    assignment 2

---
+++ calculate_abcb_rates.py
@@ -0,0 +1,43 @@
+
+def calculate(abcb_rates,amount):
+    effective_rate=0
+    remaining_amount=amount
+    for k,v in abcb_rates.items():
+        lower_limit = int(k.split("-")[0].split("$")[1])
+        upper_limit = int(k.split("-")[1].split("$")[1])
+        temp_amount = upper_limit-lower_limit
+        if (remaining_amount<upper_limit):
+            effective_rate = effective_rate + remaining_amount/amount * v
+            break
+        else:
+            effective_rate = effective_rate + (temp_amount / amount) * v
+            remaining_amount = remaining_amount - temp_amount
+
+    if remaining_amount>0:
+        effective_rate = effective_rate + (remaining_amount / amount) * 0.05
+
+
+
+    return effective_rate
+
+if __name__ == '__main__':
+    # abcb_rates = {
+    #     '$0 - $10000': 3.00,
+    #     '$10000 - $30000': 4.00,
+    # }
+    # print(calculate(abcb_rates, 40000))
+    #
+    #
+    # abcb_rates = {
+    #     '$0 - $10000': 2.00,
+    #     '$10000 - $30000': 2.50,
+    #     '$30000 - $90000': 3.00,
+    #     '$90000 - $100000': 7.00
+    # }
+    # print(calculate(abcb_rates, 50000))
+
+    abcb_rates = {
+        '$0 - $50000': 2.88,
+        '$50000 - $51000': 9.18,
+    }
+    print(calculate(abcb_rates, 100000))
---
+++ index_v2.py
@@ -0,0 +1,32 @@
+
+
+def index_v2(list_of_values, value):
+    result=[]
+
+    for i in range(0,len(list_of_values)):
+        if list_of_values[i]==value:
+            result.append(i)
+
+    return result
+
+if __name__ == '__main__':
+    list_of_values = [1, 3, 4, 1, 5, 2]
+    print(range(len(list_of_values)))
+    value = 1
+    print(index_v2(list_of_values, value))
+
+    #this was returing [1,1] the actual value but not the index
+    # result = [num for num in list_of_values if num==value]
+    # print(result)
+
+    # this is correct using lis comprehension
+    result = [i for i in range(len(list_of_values)) if list_of_values[i] == value]
+    print(result)
+
+    # Using Lambda
+    result = list(filter(lambda i: list_of_values[i] == value, range(len(list_of_values))))
+    print(result)
+
+
+
+# How to do this with list comprehension
