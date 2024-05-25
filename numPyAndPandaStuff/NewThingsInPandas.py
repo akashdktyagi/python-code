d76ab7e2cb991d325d91225fb7212551f7a05018
akashdktyagi <akashdktyagi@gmail.com>
2024-05-26 02:12:07 +0800

    pandas juypter

---
+++ numPyAndPandaStuff/NewThingsInPandas.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+if __name__=="__main__":
+    df = pd.DataFrame(
+        {'first name': ['Josh', 'John', 'Jane',"a","b","c"],
+         'last name': ['Anderson', 'Doe', 'Doe',"e","f","g"]
+         }
+    )
+
+    print(df)
+    df['last name'].drop([1,2],inplace=True)
+
+    # df['last name'] = df['last name'].apply(str.title)
+
+    # print(df)
+
+    print(df)
